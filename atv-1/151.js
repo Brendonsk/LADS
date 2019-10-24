1264d3384485cfd96b9d44a8cb8703a2bf37cc08
Brendon <brendon@gmail.com>
2019-10-24 17:49:18 -0400

    151

--- atv-1/151.js
+++ atv-1/151.js
@@ -1,1 +1,13 @@
-console.log("pasta")
+/**
+ * Função que recebe um número inteiro inserido pelo usuário e retorna verdadeiro caso ele seja par, e falso caso ele seja ímpar
+ */
+let paridade = function() {
+    n = prompt("Insira um numero inteiro: ")//janela de prompt que requisita ao usuário que insira um número
+    if (n%2==0) {//operador de resto(%) utilizado para verificar se o resto da divisão do número inserido é par, caso seja 0, e ímpar, caso seja 1
+        return(true);
+    } else {
+        return(false);
+    }
+};
+
+paridade();//execução da função paridade
