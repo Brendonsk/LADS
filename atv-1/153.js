cf81922355df313f5ec2587ec0c2b1b6941fd939
Brendon <brendon@gmail.com>
2019-10-28 10:00:09 -0400

    adicionado o exercício 153 da atividade 1, devidamente comentado

---
+++ atv-1/153.js
@@ -0,0 +1,13 @@
+tempF = prompt("Insira a temperatura em graus Fahrenheit: "); //janela de prompt que requisita ao usuário que insira um número
+
+/**Função que calcula e retorna uma temperatura em graus Celsius dado uma temperatura em graus Fahrenheit
+ * 
+ * @param {*} F Temperatura em graus Fahrenheit
+ */
+let C = function(F){
+    return ((F-32)/9)*5;
+}
+
+alert("Temperatura em graus Celsius: " + C(tempF).toFixed(1));
+//.toFixed(a) = função que retorna o valor numérico com precisão de "a" casas decimais
+//alert("x") = exibe uma caixa de diálogo para o usuário com a string "x" dentro dela
