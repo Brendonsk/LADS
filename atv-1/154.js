385ba7b2b918c0991473ac119b623e8a3474fe6f
Brendon <brendon@gmail.com>
2019-10-28 11:24:32 -0400

    Adicionados comentários

--- atv-1/154.js
+++ atv-1/154.js
@@ -1,6 +1,11 @@
-let n1 = Number(prompt("Insira sua primeira nota: "));
-let n2 = Number(prompt("Insira sua segunda nota: "));
+let n1 = Number(prompt("Insira sua primeira nota: "));//janela de prompt que requisita ao usuário que insira um número
+let n2 = Number(prompt("Insira sua segunda nota: "));//janela de prompt que requisita ao usuário que insira um número
 
+/**Função que calcula e retorna a média das duas notas passadas como parâmetros
+ * 
+ * @param {*} N1 Primeira nota
+ * @param {*} N2 Segunda nota
+ */
 let media = function(N1,N2){
     let mensagem = "";
     sm = (N1+N2)/2;
