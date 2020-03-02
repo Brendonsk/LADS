d265fbf2f549b583c95846c54337aff7bc0078fc
Brendonsk <brendonbenevides@hotmail.com>
2020-03-02 13:54:51 -0400

    Adicionado a pagina em html, estilo em css, e modificado o programa em js da atividade 152

--- atv-1/152.js
+++ atv-1/152.js
@@ -2,6 +2,8 @@
  * 
  * @param {*} raio Raio da circunferência
  */
+
+
 let area = function(raio){
   //Math.pow(a,b) = função da biblioteca Math que calcula a base "a" elevada ao expoente "b"
   //Math.PI = constante da biblioteca Math equivalente ao número irracional "pi"
@@ -20,8 +22,20 @@ let calculos = function(){
   raio = document.getElementById("formulario").raio.value;
   if(raio){
     raio = parseFloat(raio);
-    document.getElementById("area").innerHTML = "Área = "+area(raio).toFixed(2);
-    document.getElementById("perimetro").innerHTML = "Perímetro = "+perimetro(raio).toFixed(2);
+    let area_valor = area(raio);
+    let perimetro_valor = perimetro(raio);
+
+    document.getElementById("perimetro").innerHTML = "Perímetro = "+perimetro_valor.toFixed(2);
+
+    if(isFinite(area_valor)){ //area e perimetro sao finitos
+      document.getElementById("area").innerHTML = "Área = "+area_valor.toFixed(2);
+    }
+    else{ //area infinita
+      document.getElementById("area").innerHTML = "Área muito grande";
+    }
+  }
+  else{
+    alert("O campo não pode estar vazio!")
   }
 }
 //.toFixed(a) = função que retorna o valor numérico com precisão de "a" casas decimais
